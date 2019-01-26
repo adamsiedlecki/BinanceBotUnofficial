0a5a13cf11db633848b1085467f5200958029537
adamsiedlecky <adamsiedlec@wp.pl>
2019-01-26 21:35:08 +0100

    I added a class that will be usefull to hide souts from Api;

---
+++ src/main/java/Tools/StandardOutputChanger.java
@@ -0,0 +1,22 @@
+package Tools;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class StandardOutputChanger {
+
+    private static PrintStream originalPrintStream = System.out;
+
+    public static void closeOutput(){
+        originalPrintStream = System.out;
+        System.setOut(new PrintStream(new OutputStream() {
+            public void write(int b) {
+                // NO-OP
+            }
+        }));
+    }
+
+    public static void openOutput(){
+        System.setOut(originalPrintStream);
+    }
+}
